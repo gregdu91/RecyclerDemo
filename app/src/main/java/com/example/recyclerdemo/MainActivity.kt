7fc5faabf9a13ae431fa020c6bf0cc0b14fc8bb7
gregdu91 <greg91400@hotmail.fr>
2020-11-05 19:52:06 +0100

    1

---
+++ app/src/main/java/com/example/recyclerdemo/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.recyclerdemo
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recycler_view.setBackgroundColor(Color.BLUE)
+    }
+}
